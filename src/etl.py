0c5241cbab4ade38f389d6c42bccbd8639f7d481
luiz-decio <luizgdecio@gmail.com>
2024-07-06 13:30:40 -0300

    feat: add etl script

---
+++ src/etl.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import pandera as pa
+
+def extract_data(path_dir: str) -> pd.DataFrame:
+    pass
+
+def transform_data(df: pd.DataFrame) -> pd.DataFrame:
+    pass
+
+def load_data(df: pd.DataFrame) -> None:
+    pass
