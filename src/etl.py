5cfec9ba8fe1e4f56dc9a4f1daeb52aa3844d811
luiz-decio <luizgdecio@gmail.com>
2024-07-08 16:45:29 -0300

    docs: add load step to etl script

--- src/etl.py
+++ src/etl.py
@@ -29,9 +29,27 @@ def transform_data(df: pd.DataFrame) -> pd.DataFrame:
     return df_transformed
 
 def load_data(df: pd.DataFrame) -> None:
-    pass
+    load_dotenv()
+
+    POSTGRES_USER = os.getenv("POSTGRES_USER")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT")
+    POSTGRES_DB = os.getenv("POSTGRES_DB")
+
+    POSTGRES_DB_URL = f"postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+
+    # Create the PostgreSQL engine and deine the table name
+    engine = create_engine(POSTGRES_DB_URL)
+    table_name = "finance_metrics"
+
+    try:
+        df.to_sql(table_name, engine, if_exists = "replace", index = False)
+    except Exception as e:
+        print(f"Error while trying to load table: {e}")
 
 if __name__ == '__main__':
     path_dir = "data/finance_data.csv"
     df = extract_data(path_dir=path_dir)
     df_transformed = transform_data(df)
+    load_data(df_transformed)
