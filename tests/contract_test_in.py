f0de9d03d67716430f7ad7b0fc02c51951e5dc0c
luiz-decio <luizgdecio@gmail.com>
2024-07-07 21:35:06 -0300

    feat: update test script

--- tests/contract_test_in.py
+++ tests/contract_test_in.py
@@ -11,7 +11,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")
 from src.contract import BaseFinanceMetrics
 
 def correct_contract_test():
-    df_test = pd.DataFrame({
+    df_test = pd.dateFrame({
         "company_sector": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
         "operating_revenue": [1000,1000,1000],
         "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
@@ -21,99 +21,100 @@ def correct_contract_test():
 
     BaseFinanceMetrics.validate(df_test)
 
-def test_coluna_adicional():
-    df_test = pd.DataFrame({
-        "setor_da_empresa": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
-        "receita_operacional": [1000,1000,1000],
-        "data": ["2023-01-01", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [0.1, 0.1, 0.1],
-        "custos_operacionais": [200,200,200],
-        "coluna_adicional": [0,0,0]
+def new_column_test():
+    df_test = pd.dateFrame({
+        "company_sector": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
+        "operating_revenue": [1000,1000,1000],
+        "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
+        "tax_percent": [0.1, 0.1, 0.1],
+        "operatiing_costs": [200,200,200],
+        "dummy_column": [0,0,0]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
 
-def test_coluna_em_falta():
-    df_test = pd.DataFrame({
-        "receita_operacional": [1000,1000,1000],
-        "data": ["2023-01-01", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [0.1, 0.1, 0.1],
-        "custos_operacionais": [200,200,200],
-        "coluna_adicional": [0,0,0]
+def missing_column_test():
+    df_test = pd.dateFrame({
+        # The company_sector column was removed to test the method
+        "operating_revenue": [1000,1000,1000],
+        "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
+        "tax_percent": [0.1, 0.1, 0.1],
+        "operatiing_costs": [200,200,200],
+        "dummy_column": [0,0,0]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
 
-def test_valor_em_falta():
-    df_test = pd.DataFrame({
-        "setor_da_empresa": [np.nan, "REP_X7Y8Z9", "MNT_4D5E6F"],
-        "receita_operacional": [1000,1000,1000],
-        "data": ["2023-01-01", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [0.1, 0.1, 0.1],
-        "custos_operacionais": [200,200,200]
+def missing_value_test():
+    df_test = pd.dateFrame({
+        "company_sector": [np.nan, "REP_X7Y8Z9", "MNT_4D5E6F"],
+        "operating_revenue": [1000,1000,1000],
+        "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
+        "tax_percent": [0.1, 0.1, 0.1],
+        "operatiing_costs": [200,200,200]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
 
-def test_setor_invalido():
-    df_test = pd.DataFrame({
-        "setor_da_empresa": ["AAA_X7Y8Z9", "REP_X7Y8Z9", "MNT_4D5E6F"],
-        "receita_operacional": [1000,1000,1000],
-        "data": ["2023-01-01", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [0.1, 0.1, 0.1],
-        "custos_operacionais": [200,200,200]
+def invalid_sector_test():
+    df_test = pd.dateFrame({
+        "company_sector": ["AAA_X7Y8Z9", "REP_X7Y8Z9", "MNT_4D5E6F"],
+        "operating_revenue": [1000,1000,1000],
+        "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
+        "tax_percent": [0.1, 0.1, 0.1],
+        "operatiing_costs": [200,200,200]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
     
-def test_receita_negativa():
-    df_test = pd.DataFrame({
-        "setor_da_empresa": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
-        "receita_operacional": [-1000,1000,1000],
-        "data": ["2023-01-01", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [0.1, 0.1, 0.1],
-        "custos_operacionais": [200,200,200]
+def negative_revenue_test():
+    df_test = pd.dateFrame({
+        "company_sector": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
+        "operating_revenue": [-1000,1000,1000],
+        "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
+        "tax_percent": [0.1, 0.1, 0.1],
+        "operatiing_costs": [200,200,200]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
 
-def test_data_invalida():
-    df_test = pd.DataFrame({
-        "setor_da_empresa": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
-        "receita_operacional": [1000,1000,1000],
-        "data": ["data invalida", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [0.1, 0.1, 0.1],
-        "custos_operacionais": [200,200,200]
+def invalid_date_test():
+    df_test = pd.dateFrame({
+        "company_sector": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
+        "operating_revenue": [1000,1000,1000],
+        "date": ["invalid date", "2023-01-01", "2023-01-01"],
+        "tax_percent": [0.1, 0.1, 0.1],
+        "operatiing_costs": [200,200,200]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
 
-def test_porcentagem_imposto():
-    df_test = pd.DataFrame({
-        "setor_da_empresa": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
-        "receita_operacional": [1000,1000,1000],
-        "data": ["2023-01-01", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [1.1, 0.1, 0.1],
-        "custos_operacionais": [200,200,200]
+def tax_percent_test():
+    df_test = pd.dateFrame({
+        "company_sector": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
+        "operating_revenue": [1000,1000,1000],
+        "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
+        "tax_percent": [1.1, 0.1, 0.1],
+        "operatiing_costs": [200,200,200]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
         
-def test_custos_negativos():
-    df_test = pd.DataFrame({
-        "setor_da_empresa": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
-        "receita_operacional": [1000,1000,1000],
-        "data": ["2023-01-01", "2023-01-01", "2023-01-01"],
-        "percentual_de_imposto": [0.1, 0.1, 0.1],
-        "custos_operacionais": [-200,200,200]
+def negative_costs_test():
+    df_test = pd.dateFrame({
+        "company_sector": ["VND_A1B2C3", "REP_X7Y8Z9", "MNT_4D5E6F"],
+        "operating_revenue": [1000,1000,1000],
+        "date": ["2023-01-01", "2023-01-01", "2023-01-01"],
+        "tax_percent": [0.1, 0.1, 0.1],
+        "operatiing_costs": [-200,200,200]
     })
 
     with pytest.raises(pa.errors.SchemaError):
-        MetricasFinanceirasBase.validate(df_test)
+        BaseFinanceMetrics.validate(df_test)
